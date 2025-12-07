8521a36bfc61b1097da71d112d4af0fe58e0627d
elmurodurod <elmurodurodmamatkulov@gmail.com>
2025-12-07 23:11:59 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// Всё настраивается здесь
+window.CONFIG = {
+    // Имя менеджера (подставляется в блоке под аватаркой)
+    managerName: "Александр",
+
+    // Номер WhatsApp БЕЗ + и пробелов, только цифры (пример: 447000000000)
+    whatsappPhone: "447000000000",
+
+    // Ник в Telegram БЕЗ @ (пример: brite_jobs_uk)
+    telegramNick: "manager_nick",
+
+    // Facebook Pixel ID (пример: 123456789012345)
+    fbPixelId: "123456789012345"
+};
