c7dc453c29240b66a969e1cff481ddf84189c27c
elmurodurod <elmurodurodmamatkulov@gmail.com>
2025-12-08 00:21:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // Всё настраивается здесь
 window.CONFIG = {
     // Имя менеджера (подставляется в блоке под аватаркой)
-    managerName: "Александр",
+    managerName: "Ян Полонский",
 
     // Номер WhatsApp БЕЗ + и пробелов, только цифры (пример: 447000000000)
-    whatsappPhone: "447000000000",
+    whatsappPhone: "447526236340",
 
     // Ник в Telegram БЕЗ @ (пример: brite_jobs_uk)
-    telegramNick: "manager_nick",
+    telegramNick: "yazagul",
 
     // Facebook Pixel ID (пример: 123456789012345)
-    fbPixelId: "123456789012345"
+    fbPixelId: "1369926611226811"
 };
